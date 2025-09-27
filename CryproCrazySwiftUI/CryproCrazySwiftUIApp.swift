fe135a97d355ac6ad9ced29a9b00a9411416c893
Sabri Demir <safvcdsa161@gmail.com>
2025-09-27 11:01:18 +0300

    Initial Commit

---
+++ CryproCrazySwiftUI/CryproCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryproCrazySwiftUIApp.swift
+//  CryproCrazySwiftUI
+//
+//  Created by Sabri Demir on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryproCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
